c08e92cbc31237623b5fdef00439fdb6d54dbe5f
Alessandro Canicatti <ales.canicatti@gmail.com>
2018-03-19 16:19:37 +0100

    Dockerfile contains env vars that will be used from application

--- docker-sample/docker-tutorial/app.py
+++ docker-sample/docker-tutorial/app.py
@@ -1,10 +1,12 @@
 import time
-
+import os
 import redis
 from flask import Flask
 
 app = Flask(__name__)
 cache = redis.Redis(host='redis', port=6379)
+site_host = os.environ['HOST']
+site_port = int(os.environ['PORT'])
 
 def get_hit_count():
     retries = 5
@@ -20,8 +22,12 @@ def get_hit_count():
 
 @app.route('/')
 def hello():
+    return 'Hello World!'
+
+@app.route('/count')
+def count():
     count = get_hit_count()
     return 'Hello World! These are my greetings... I have been seen {} times.\n'.format(count)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", debug=True)
+    app.run(host=site_host, port=site_port, debug=True)
